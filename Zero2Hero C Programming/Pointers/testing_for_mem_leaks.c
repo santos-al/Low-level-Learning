a7b79202103fe8b9b2441c1816ff79febfcfdb57
Alexandre Santos <santosal2018@gmail.com>
2025-05-04 15:24:47 +0100

    testing memory leaks

---
+++ Zero2Hero C Programming/Pointers/testing_for_mem_leaks.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+#define MAX_EMPLOYEES 1000
+
+// dont create global variables because they can be hard to track
+// and cause issues
+int g_numEmployees = 0;
+
+struct employee_t {
+  int id;
+  int income;
+  int staff;
+
+};
+
+int intialize_employee(struct employee_t *e) {
+  static int numEmployees = 0;
+  numEmployees++;
+
+  e->id = numEmployees;
+  e->income = 0;
+  e->staff = false;
+
+  return numEmployees;
+}
+
+int main() {
+  
+  int n = 4;
+
+  struct employee_t *employees = malloc(sizeof(struct employee_t)*n);
+  if (employees == NULL) {
+    printf("The allocator failed\n");
+    return -1;
+  }
+
+  for (int i = 0; i < n; i++) {
+    int id = intialize_employee(&employees[i]);
+    printf("New Employee, ID is %d\n", id);
+  }
+  free(employees); 
+  employees = NULL;
+
+}
